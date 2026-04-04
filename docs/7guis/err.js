8b7ff723513880d309c2c64a46e01bdfb95c753c
DOSAYGO Engineering <development.team@dosyago.com>
2026-04-04 20:42:33 +0800

    ok

--- docs/7guis/err.js
+++ docs/7guis/err.js
@@ -1,1 +1,31 @@
-/* Error hook placeholder for 7GUIs docs page. */
+
+  {
+    const DEBUG = {
+      dev: true, 
+      err: true
+    };
+
+    setupErrorCatchers();
+
+    function setupErrorCatchers() {
+      (DEBUG.dev || DEBUG.err) && (self.onerror = (...v) => (func(v), true));
+      (DEBUG.dev || DEBUG.err) && (self.onunhandledrejection = (e) => (e.promise.catch(err => func(err+' '+err.stack)), true));
+    }
+
+    function func() {
+      if ( isMobile() ) {
+        return (...x) => alert(x[0]);
+      } else {
+        return (...x) => console.log(...x)
+      }
+    }
+
+    function extractMeat(list) {
+      const meatIndex = list.findIndex(val => !! val && val.message || val.stack);
+      if ( meatIndex == -1 || meatIndex == undefined ) {
+        return "";
+      } else {
+        return list[meatIndex];
+      }
+    }
+  }
